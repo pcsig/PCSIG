baff4609c1ca2b4027a27e36ce84700525ee1ef5
Cleber Brito <clebercbs@gmail.com>
2020-05-23 16:15:18 -0300

    Update canal.py

--- canal.py
+++ canal.py
@@ -22,24 +22,21 @@ lider_socket.bind((host, 44443))
 timestamp = time.time()
 colisoes = []
 conjunto_colisoes = set()
-latencia = 0.02701
-t_aleatorio = random.uniform(0.001,0.003)
+latencia = 0.025
 l_msn = []
 q = queue.Queue()
+tam_fila = 0
 
 def buffer():
     global perda
+    global tam_fila
     while True:
         data, addr = lider_socket.recvfrom(4096)
         recb_canal = json.loads(data)
         if recb_canal:
             q.put(recb_canal)
-        # ###########################  Contabilizando as colisões ###########################
-        #     with open('colisoes.txt', 'a') as arq:
-        #         arq.write(str('{0:.6f}'.format(time.time())))
-        #         arq.write(',')
-        # ###################################################################################
-
+            tam_fila = q.qsize()
+            print tam_fila
         ######################### Fila #########################
         with open('fila.csv', 'a') as arq:
             arq.write(str(recb_canal[0]))
@@ -49,13 +46,14 @@ def buffer():
         ########################################################
 
 def encaminhar():
+    global tam_fila
     while True:
         if not q.empty():
-            time.sleep(latencia+t_aleatorio)
+            time.sleep(latencia)
             mensagem = q.get()
             resultado = np.arange (start = 1, stop = 3)
             perdaDeMensagens = np.random.choice (a = resultado, p = [1-perda, perda])
-            if perdaDeMensagens == 1:
+            if perdaDeMensagens == 1 and tam_fila < 20:
                 message = json.dumps(mensagem)
                 lider_socket.sendto(message.encode(), (host, 44444))
             else:
